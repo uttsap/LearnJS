4cbd3b0a57cfad6790a290f458ee6ba82260efa8
Uttam Sapkota <usapkota@gmail.com>
2018-05-05 16:15:52 +0545

    first controller

---
+++ app.js
@@ -0,0 +1,9 @@
+var budgetController = (function() {
+    
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };
+    
